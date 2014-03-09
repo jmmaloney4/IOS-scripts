e75534ba674ea3d47e885b3ef6ac6efa1dfd8595
jmmaloney4 <jmmaloney4@gmail.com>
2014-03-09 10:52:40 -0500

    getecid update

--- getecid.cpp
+++ getecid.cpp
@@ -1,14 +1,16 @@
 //  Created by Jack Maloney on 3/8/14.
 //  Copyright (c) 2014 Jack Maloney. All rights reserved.
 
+#import <iostream>
 #import <stdio.h>
 
 int main(int argc, const char * argv[])
 {
+    std::cout << "Please put your iDevice into recovery before running this command"
     FILE *in;
     in = popen("system_profiler SPUSBDataType | sed -n -e '/Apple Mobile Device (Recovery Mode)/,/Serial/p' | grep 'ECID:' | awk -F ': ' '{print $2}'", "r");
     while (TRUE) {
-        
+
     }
     return 0;
 }
