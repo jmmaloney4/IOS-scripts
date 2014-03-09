675d7d3ce9204c462e0032a7ee894006f821f7ad
jmmaloney4 <jmmaloney4@gmail.com>
2014-03-08 19:06:01 -0600

    fixed a compile error with getudid.cpp

--- getudid.cpp
+++ getudid.cpp
@@ -1,8 +1,11 @@
 //  Created by Jack Maloney on 3/8/14.
 //  Copyright (c) 2014 Jack Maloney. All rights reserved.
 
+#import <iostream>
+#import <stdio.h>
+
 int main(int argc, const char * argv[])
 {
-    system("system_profiler SPUSBDataType | sed -n -e '/iPad/,/Serial/p' -e '/iPhone/,/Serial/p' | grep "Serial Number:" | awk -F ": " '{print $2}'");
+    system("system_profiler SPUSBDataType | sed -n -e '/iPad/,/Serial/p' -e '/iPhone/,/Serial/p' | grep 'Serial Number:' | awk -F ': ' '{print $2}'");
     return 0;
 }
